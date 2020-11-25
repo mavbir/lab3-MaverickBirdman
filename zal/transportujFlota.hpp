88f4f64d05c3d92d6d58fd9a73024857e3a1841d
Adam Chełchowski <74993250+mavbir@users.noreply.github.com>
2020-11-25 01:47:46 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,44 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+	int unsigned static liczbaZaglowcow=0;
+	int unsigned static licznikPrzetransportowanegoSumarycznieTowaru;
+	//#1
+	Stocznia s{};
+	Statek* s1 = s();
+	//#dod
+	Zaglowiec* za = dynamic_cast<Zaglowiec*>(s1);
+	if (za != nullptr)
+	{
+		liczbaZaglowcow = ++liczbaZaglowcow;
+	}
+	//#2 + #3
+	licznikPrzetransportowanegoSumarycznieTowaru = licznikPrzetransportowanegoSumarycznieTowaru + s1->transportuj();
+	//#4
+	delete s1;
+	//#5
+	if (towar == 0)
+	{
+		cout << "Brak towaru do transportu" << endl;
+	}
+	else
+	{
+		while (licznikPrzetransportowanegoSumarycznieTowaru < towar)
+		{
+			//#1
+			Stocznia s{};
+			Statek* s1 = s();
+			//#2 + #3
+			licznikPrzetransportowanegoSumarycznieTowaru = licznikPrzetransportowanegoSumarycznieTowaru + s1->transportuj();
+			//#4
+			delete s1;
+		}
+	}
+
+
+	return liczbaZaglowcow;
+    
+    
+    
+    //return 0;
 }
